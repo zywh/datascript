81f464d57ee00fdb19a3c625e12d5807fcd03cc2
David Zheng <zhengying@yahoo.com>
2016-11-02 11:10:01 -0500

    autobackup freelifescript 2016110211

--- count_crea_pic_num.sh
+++ count_crea_pic_num.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 sqlfile="/tmp/crea_pic_num.txt"
-cd /mls/crea
+crea_pic_prefix="/disk2/crea"
 sudo rm $sqlfile
-#du -a | cut -d/ -f2 | sort | uniq -c >/tmp/treb_pic_count.raw
-cat /tmp/treb_pic_count.raw | while read line
+echo `date` >/tmp/test_eval
+
+
+function scan_pic  {
+province=$1
+cd $crea_pic_prefix/$province
+echo "$crea_pic_prefix/$province"
+
+du -a | cut -d/ -f2 | sort | uniq -c >/tmp/crea_pic_count.raw
+cat /tmp/crea_pic_count.raw | while read line
 do
 set $line
 count=$1
@@ -12,18 +20,29 @@ ml_num=`echo $2 | sed 's/Photo//'`
 ((count = count - 1))
 if [ -n "$ml_num" ]
 then
-#echo "update h_house set pic_num='"$count"' where ml_num='"$ml_num"';" >>/tmp/pic_num.sql
 echo $ml_num","$count >>$sqlfile
 
 fi
 done
+
+}
+
+
+scan_pic  Ontario "Ontario"
+scan_pic  BritishColumbia "British Columbia"
+scan_pic  Alberta "Alberta"
+scan_pic  NewfoundlandLabrador 'Newfoundland & Labrador'
+scan_pic  PrinceEdwardIsland 'Prince Edward Island'
+scan_pic  NewBrunswick 'New Brunswick'
+scan_pic  NovaScotia 'Nova Scotia'
+
+echo `date` >>/tmp/test_eval
+exit 0
 
 sudo chown mysql:mysql $sqlfile
 #sqlcmd="mysql -u hdm106787551 -h  alinew -pMaplemYsql100 --local-infile  hdm106787551_db "
 loadsql="LOAD DATA INFILE '"$sqlfile"'  replace INTO TABLE pic_num   FIELDS TERMINATED BY ',' ;"
 
 /usr/bin/mysql -u root -p19701029 mls -e "$loadsql"
-
-
 
 
