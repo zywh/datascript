099723628fc94ee212306706dfd229526d33d87e
David Zheng <zhengying@yahoo.com>
2016-11-03 15:10:01 -0500

    autobackup freelifescript 2016110315

--- cleanpic.sh
+++ cleanpic.sh
@@ -19,8 +19,8 @@ condopic="/mls/treb"
 
 
 #Get Current Active List from table
-#mysql -u root -p19701029 -N -B mls -e 'select ml_num from vowresi where status="A"' >/tmp/mls_a
-#mysql -u root -p19701029 -N -B mls -e 'select ml_num from vowcondo where status="A"' >>/tmp/mls_a
+mysql -u root -p19701029 -N -B mls -e 'select ml_num from vowresi where status="A"' >/tmp/mls_a
+mysql -u root -p19701029 -N -B mls -e 'select ml_num from vowcondo where status="A"' >>/tmp/mls_a
 cat $homedir/vowcondo/data/avail.txt $homedir/vowresi/data/avail.txt >/tmp/mls_a
 cp /tmp/mls_a /tmp/activemls
 ls $resipic | sed 's/Photo//'  >>/tmp/mls_a
