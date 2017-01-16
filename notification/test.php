1a0f7d36ab215ed3a2700b32481fa9c2a40d7624
David Zheng <zhengying@yahoo.com>
2017-01-16 13:10:02 -0500

    autobackup freelifescript 2017011613

--- notification/test.php
+++ notification/test.php
@@ -1,42 +1,65 @@
 <?php
 
-require_once('vendor/autoload.php');
-use \Firebase\JWT\JWT;
-$key = "example_key";
-$token = array(
-    "iss" => "http://example.org",
-    "aud" => "http://example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
-);
 
-/**
- * IMPORTANT:
- * You must specify supported algorithms for your application. See
- * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
- * for a list of spec-compliant algorithms.
- */
-$jwt = JWT::encode($token, $key);
-print_r($jwt);
-$decoded = JWT::decode($jwt, $key, array('HS256'));
+// Payload data you want to send to Android device(s)
+// (it will be accessible via intent extras)    
+$data = array('message' => 'Hello World!');
 
-print_r($decoded);
+// The recipient registration tokens for this notification
+// https://developer.android.com/google/gcm/    
+$ids = array('abc', 'def');
 
-/*
- NOTE: This will now be an object instead of an associative array. To get
- an associative array, you will need to cast it as such:
-*/
+// Send push notification via Google Cloud Messaging
+sendPushNotification($data, $ids);
 
-$decoded_array = (array) $decoded;
+function sendPushNotification($data, $ids) {
+    // Insert real GCM API key from the Google APIs Console
+    // https://code.google.com/apis/console/        
+    $apiKey = getenv("GCM_APIKEY");
 
-/**
- * You can add a leeway to account for when there is a clock skew times between
- * the signing and verifying servers. It is recommended that this leeway should
- * not be bigger than a few minutes.
- *
- * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
- */
-JWT::$leeway = 60; // $leeway in seconds
-$decoded = JWT::decode($jwt, $key, array('HS256'));
+    // Set POST request body
+    $post = array(
+                    'registration_ids'  => $ids,
+                    'data'              => $data,
+                 );
+
+    // Set CURL request headers 
+    $headers = array( 
+                        'Authorization: key=' . $apiKey,
+                        'Content-Type: application/json'
+                    );
+
+    // Initialize curl handle       
+    $ch = curl_init();
+
+    // Set URL to GCM push endpoint     
+    curl_setopt($ch, CURLOPT_URL, 'https://gcm-http.googleapis.com/gcm/send');
+
+    // Set request method to POST       
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    // Set custom request headers       
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    // Get the response back as string instead of printing it       
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Set JSON post data
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
+    // Actually send the request    
+    $result = curl_exec($ch);
+
+    // Handle errors
+    if (curl_errno($ch)) {
+        echo 'GCM error: ' . curl_error($ch);
+    }
+
+    // Close curl handle
+    curl_close($ch);
+
+    // Debug GCM response       
+    echo $result;
+}
 
 ?>
